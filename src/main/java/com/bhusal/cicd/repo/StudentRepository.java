0ec476f2cc7bc656a4b8e0cba3ba1458bb7109da
bishnu20 <bhusalb0@gmail.com>
2026-06-21 12:47:48 -0600

    first commit in the branch

---
+++ src/main/java/com/bhusal/cicd/repo/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.bhusal.cicd.repo;
+
+import com.bhusal.cicd.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+}
