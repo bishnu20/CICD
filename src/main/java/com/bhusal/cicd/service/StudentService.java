e26689502627687ce66d9091014e116f3b0e0a3e
bishnu20 <bhusalb0@gmail.com>
2026-06-21 13:06:48 -0600

    service class added

---
+++ src/main/java/com/bhusal/cicd/service/StudentService.java
@@ -0,0 +1,11 @@
+package com.bhusal.cicd.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * This class serves as the service layer for managing student-related operations. It will contain business logic and interact with the StudentRepository to perform CRUD operations on Student entities.
+ */
+@Service
+public class StudentService {
+
+}
